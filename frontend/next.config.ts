5b025c61798385e33f42a716135925d912b2afa5
Matthew Li <matthewlischoolemail@gmail.com>
2024-11-24 00:59:32 -0500

    working postgres upload

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  future: { webpack5: true },
   webpack: (config, {}) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
