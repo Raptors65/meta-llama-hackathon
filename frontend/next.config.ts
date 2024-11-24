28392defa7034c0de605369625b3879739b3a7b7
Lucas Kim <lucaskamadakim@gmail.com>
2024-11-24 01:57:50 -0500

    Revert "Merge branch 'main' into follow-ups"
    This reverts commit 3e4a3d6fffb5bf907a9bb42aac0a90b985db02f9, reversing
    changes made to eb183d4661c1f7630f79f4410d71e41512fa903a.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  future: { webpack5: true },
   webpack: (config, {}) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
